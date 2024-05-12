9b23b43fa633b0ac167ecfe403efb5c8bf357209
Gustavo Alves <medeirosgustavo.a@gmail.com>
2024-05-11 23:48:06 -0300

    projeto conta finalizado

--- consta-banco/src/contaTerminal.java
+++ consta-banco/src/contaTerminal.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class contaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Scanner scanner = new Scanner(System.in);
+
+        double saldo = 15;
+
+        System.out.println("Digite o valor para saque: ");
+        double valorSolicitado = scanner.nextDouble();
+
+        if(saldo >= valorSolicitado) {
+
+            saldo -= valorSolicitado;
+
+        } else if (valorSolicitado == 0){
+            
+            System.out.println("Valor precisa ser maior que zero.");
+            
+        } else {
+            
+            System.out.println("Saldo insuficiente para saque.");
+        }
+
+
+        System.out.println("Saldo atual: " + saldo);
     }
 }
